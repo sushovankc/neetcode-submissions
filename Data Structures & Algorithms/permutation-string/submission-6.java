20921c39e9b8468b30d1bed8bcf754f2e8eafe08
Sushovan KC <46551330+sushovankc@users.noreply.github.com>
2026-08-11 21:54:36 +0530

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length() > s2.length()) return false;
+
+        int[] need = new int[26];
+        int[] window = new int[26];
+
+        for(char c : s1.toCharArray()){
+            need[c - 'a']++;
+        }
+
+        int m = s1.length();
+        for(int i = 0; i < s2.length(); i++){
+            window[s2.charAt(i) - 'a']++;
+            if(i >= m){
+                window[s2.charAt(i-m) - 'a']--;
+            }
+            if( i - m + 1 >= 0 && Arrays.equals(need,window)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
