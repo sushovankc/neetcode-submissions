9633ffed825f8da0cf017a0766e48878a60c3f91
Sushovan KC <46551330+sushovankc@users.noreply.github.com>
2026-09-04 22:46:44 +0530

    Add: find-median-in-a-data-stream - submission-9

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-9.java
@@ -0,0 +1,52 @@
+class MedianFinder {
+    
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>(); // Stores larger half
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder()); // stores smaller half
+    
+    public MedianFinder() {
+        
+    }
+    
+    public void addNum(int num) {
+
+        // if(minHeap.isEmpty() && maxHeap.isEmpty()){
+        //     minHeap.add(num);
+        //     return;
+        // }
+
+        // if(num > minHeap.peek()){
+        //     minHeap.add(num);
+        // }else{
+        //     maxHeap.add(num);
+        // }
+
+        if(maxHeap.isEmpty() || num <= maxHeap.peek()){
+            maxHeap.add(num);
+        }else{
+            minHeap.add(num);
+        }
+
+        if(Math.abs(minHeap.size()-maxHeap.size())>1){
+            if(minHeap.size() > maxHeap.size()){
+                maxHeap.add(minHeap.poll());
+            }else{
+                minHeap.add(maxHeap.poll());
+            }
+        }
+        
+    }
+    
+    public double findMedian() {
+
+        if(maxHeap.size() > minHeap.size()){
+            return maxHeap.peek();
+        }
+
+        if(minHeap.size() > maxHeap.size()){
+            return minHeap.peek();
+        }
+
+        return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        
+    }
+}
